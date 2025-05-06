8a813f1d9e0bcaef8117a06f7acbfdcd25eb9ecc
FerdinandoPH <perez.holguin@gmail.com>
2025-05-06 17:06:08 +0200

    primer paso

--- PL4_c1_maestro2.sql
+++ PL4_c1_maestro2.sql
@@ -1,6 +1,11 @@
-\c postgres
+\c telpark
+BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS postgres_fdw;
+
+DROP SCHEMA IF EXISTS telpark_foraneo CASCADE;
+DROP SERVER IF EXISTS maestro1 CASCADE;
+
 CREATE SERVER IF NOT EXISTS maestro1
     FOREIGN DATA WRAPPER postgres_fdw
     OPTIONS (host 'localhost', port '5432', dbname 'telpark');
@@ -9,4 +14,11 @@ CREATE USER MAPPING IF NOT EXISTS FOR postgres
     SERVER maestro1
     OPTIONS (user 'postgres', password 'postgres');
 
-CREATE SCHEMA IF NOT EXISTS telpark_foraneo
+CREATE SCHEMA IF NOT EXISTS telpark_foraneo;
+
+IMPORT FOREIGN SCHEMA public
+    FROM SERVER maestro1
+    INTO telpark_foraneo;
+
+COMMIT;
+
